5cb9ef1a141369b9db2d9db3d65f3ee4bc18170a
MattFofo <matteo.segattini2@gmail.com>
2022-05-04 16:01:31 +0200

    stampato a schermo titoli film

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Movie;
-
 class PageController extends Controller
 {
     public function index() {
         $movies = Movie::all();
-        return view('guest.home', $movies);
-        dd($movies);
+        $data = [
+            'movies' => $movies
+        ];
+        return view('guest.home', $data);
     }
 }
