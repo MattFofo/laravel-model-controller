c195d9b442876f574290a459bc9bfa85e489c172
MattFofo <matteo.segattini2@gmail.com>
2022-05-04 16:47:10 +0200

    sass base

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -5,11 +5,19 @@
 
 @section('content')
     <h1>Home Page</h1>
-    <ul>
-        @foreach ($movies as $movie)
-            <li>{{ $movie->title }}</li>
-        @endforeach
+    <div>
+        <ul>
+            @foreach ($movies as $movie)
+                <li>
+                    <h3>Titolo &#8594; {{ $movie->title }}</h3>
+                    <h4>Titolo originale &#8594; {{ $movie->original_title }}</h4>
+                    <h4>Nazionalità &#8594; {{ $movie->nationality }}</h4>
+                    <h4>Data &#8594; {{ $movie->date }}</h4>
+                    <h4>Voto &#8594; {{ $movie->vote }}</h4>
+                </li>
+            @endforeach
 
-    </ul>
+        </ul>
+    </div>
 
 @endsection
